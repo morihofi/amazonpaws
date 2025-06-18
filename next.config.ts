8bfdf3d4bf1272e667cfec9ad1c1856aad0a88b1
Simon Biewald <simon@fam-biewald.de>
2025-06-18 16:42:54 +0200

    Add source maps to next.config.ts.
    Relates-To: 836da6b

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+
+    // Enable source maps for client and server.
+    productionBrowserSourceMaps: true,
+    webpack: (config, {isServer}) => {
+        if (isServer) {
+            config.devtool = 'source-map'
+        }
+        return config
+    },
 };
 
 export default nextConfig;
-
 
 module.exports = {
     output: 'standalone',
